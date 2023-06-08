d574f1a3054bedea9b0f0b5e97453c1aeaeaefa3
hellojyoti01 <jyotiprakash.panigrahi055@gmail.com>
2023-06-08 10:43:00 +0530

    fix: :art:

--- src/layout/DefaultLayout.js
+++ src/layout/DefaultLayout.js
@@ -1,17 +1,18 @@
 import React from 'react'
-import { AppContent, AppSidebar, AppFooter, AppHeader } from '../components/index'
+import { AppContent, AppSidebar, AppFooter, AppHeader } from '../containers/index'
 
 const DefaultLayout = () => {
   return (
     <div>
-      <AppSidebar />
+      {/* //<AppSidebar />
       <div className="wrapper d-flex flex-column min-vh-100 bg-light">
         <AppHeader />
         <div className="body flex-grow-1 px-3">
           <AppContent />
         </div>
         <AppFooter />
-      </div>
+      </div> */}
+      helllo
     </div>
   )
 }
