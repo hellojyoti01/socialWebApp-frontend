8635f6de98de0fd55273b51b1b6c81ae8a679730
hellojyoti01 <jyotiprakash.panigrahi055@gmail.com>
2023-06-09 14:52:38 +0530

    fix: :zap:

--- src/components/content/story/index.js
+++ src/components/content/story/index.js
@@ -1,7 +1,23 @@
 import React from 'react'
 import s from './story.module.css'
 function Story() {
-  return <div>Story</div>
+  const arr = [1, 2, 3, 4, 5, 6]
+  return (
+    <div className={s.container}>
+      <div className={s.own_wrapper}>
+        <div className={s.story}>Story</div>
+      </div>
+      <div className={s.friend_wrapper}>
+        {arr.map((el) => {
+          return (
+            <div className={s.story} key={el}>
+              Story
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default Story
