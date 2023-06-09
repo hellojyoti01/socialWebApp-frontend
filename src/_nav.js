de5e567c78eec37a4371761526c45053fe3158f6
hellojyoti01 <jyotiprakash.panigrahi055@gmail.com>
2023-06-09 11:15:07 +0530

    fix:

--- src/_nav.js
+++ src/_nav.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import CIcon from '@coreui/icons-react'
 import { BsHouseDoor } from 'react-icons/bs'
-import { MdOutlineTravelExplore } from 'react-icons/md'
+import { MdOutlineTravelExplore, MdOutlineNotificationsActive } from 'react-icons/md'
 import { CiSaveDown2 } from 'react-icons/ci'
 import { LuMessagesSquare } from 'react-icons/lu'
 import { CgTrending } from 'react-icons/cg'
@@ -40,9 +40,9 @@ const _nav = [
   },
   {
     component: CNavItem,
-    name: 'Trends',
-    to: '/trends',
-    icon: <CgTrending />,
+    name: 'Notification',
+    to: '/notification',
+    icon: <MdOutlineNotificationsActive />,
     //items:[]
   },
   {
