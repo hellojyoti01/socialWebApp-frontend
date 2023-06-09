ad33406e3d1245b379cc533ae1bda404546332b9
hellojyoti01 <jyotiprakash.panigrahi055@gmail.com>
2023-06-09 15:24:40 +0530

    feat: :zap:

--- src/components/content/feed/index.js
+++ src/components/content/feed/index.js
@@ -1,7 +1,29 @@
 import React from 'react'
 import s from './feed.module.css'
 function Feed() {
-  return <div>Feed</div>
+  return (
+    <div className={s.container}>
+      <div className={s.top}>
+        <h4>Feed</h4>
+        <div className={s.btn_group}>
+          <button>
+            Latest
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+          <button>
+            Popular
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </button>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Feed
